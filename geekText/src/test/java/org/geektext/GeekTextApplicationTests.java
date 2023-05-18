093f21494dc3a7aad052b86bbede0078686feb30
Isaac Del Castillo <52974036+IsaacDC@users.noreply.github.com>
2023-05-18 14:20:27 -0400

    Temp SpringBoot
    I started a springboot just to get started

---
+++ geekText/src/test/java/org/geektext/GeekTextApplicationTests.java
@@ -0,0 +1,13 @@
+package org.geektext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekTextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
